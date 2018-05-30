03289586eb00a1c028035381c3c7196c10f9b8ec
Erez Sobel <sobelerez@gmail.com>
2018-05-30 22:18:04 +0200

    Add server

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const api = require("./routes/api");
+const { errorsMiddleware } = require("./routes/api");
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", api);
+app.use(errorsMiddleware);
+
+app.get("*", (req, res, next) => {
+  const err = new Error("Page Not Found");
+  err.statusCode = 404;
+  next(err);
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Running app on http://localhost:${port}`);
+  }
+});
+
+module.exports = app;
