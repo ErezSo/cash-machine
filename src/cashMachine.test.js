8901cbf08e96bdf9a862aa1cccc66414590029ed
Erez Sobel <sobelerez@gmail.com>
2018-05-30 23:01:21 +0200

    Add tests for cashMachine

---
+++ src/cashMachine.test.js
@@ -0,0 +1,16 @@
+const cashMachine = require("./cashMachine");
+const { notes } = require("./cashMachine");
+
+it("should return the least amount of notes per value", () => {
+  const value = "60.00";
+  const expected = ["50.00", "10.00"];
+  const actual = cashMachine(value, notes);
+  expect(actual).toEqual(expected);
+});
+
+it("should empty set if value is not right", () => {
+  const value = 0;
+  const expected = [];
+  const actual = cashMachine(value, notes);
+  expect(actual).toEqual(expected);
+});
